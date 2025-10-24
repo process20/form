1b2923f5e2aa0b7a55da531325270025fbbb914b
process20 <68159663+process20@users.noreply.github.com>
2025-10-24 23:07:23 +0200

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://form-ggs1.onrender.com:5000/api';
 
 // Create axios instance with default config
 const api = axios.create({
