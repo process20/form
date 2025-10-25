2f40fe23eb81b7257c516601d546ac68a0da7132
process20 <68159663+process20@users.noreply.github.com>
2025-10-25 12:50:38 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Handle React routing, return all requests to React app
-app.get('/*', (req, res) => { res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))});
+app.get('/:path(*)', (req, res) => { res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
