754b318421fc8dcb9c2ec72de3296980882f3431
process20 <68159663+process20@users.noreply.github.com>
2025-10-25 12:32:41 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ const { fileURLToPath } = require("url");
 
 dotenv.config()
 const app = express();
-const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 
 // Connect to MongoDB
